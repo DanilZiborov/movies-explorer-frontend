aecdb6efa0fd6a3314557d1705fe4eb1f748b7a7
Danil Ziborov <ziborov.daniel@yandex.ru>
2023-09-13 17:45:48 +0300

    make promo section

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Header from '../Header/Header';
+import Main from '../Main/Main';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <div className='page'>
 
       <Header isLoggedIn={isLoggedIn}/>
+      <Main/>
 
     </div>
 
